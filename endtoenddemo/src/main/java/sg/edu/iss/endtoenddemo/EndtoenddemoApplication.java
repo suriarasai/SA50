032073a1d98033a61a8c471b00190d1c77fccad9
suriarasai <suria.r.asai@gmail.com>
2020-06-23 11:04:46 +0800

    Start of Project @ 11:04 23 June 2020

---
+++ endtoenddemo/src/main/java/sg/edu/iss/endtoenddemo/EndtoenddemoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.endtoenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndtoenddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndtoenddemoApplication.class, args);
+	}
+
+}
---
+++ endtoenddemo/src/test/java/sg/edu/iss/endtoenddemo/EndtoenddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.endtoenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndtoenddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
